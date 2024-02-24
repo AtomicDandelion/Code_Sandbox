6cb70cad073ef3667fc79b7fb091a191f6e9603a
Atomic Dandelion <pwninjakhan@gmail.com>
2024-02-24 17:19:17 -0500

    Adding resume builder script, currently configured with generic IO

---
+++ resume_builder.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+from mailmerge import MailMerge
+
+# FULL path to template with mailmerge fields
+template = "[TEMPLATE_NAME].docx"
+
+# These strings tailor the experience within the resume towards a specific field in the career statement section
+sw_string = "[SW_PITCH]"
+cyber_string = "[CYBER_PITCH]"
+
+# repeat this part as needed-----------------------------------------
+document = MailMerge(template)
+document.merge(
+	Position='[INSERT_POSITION_TITLE]',
+	Company='[INSERT_COMPANY_NAME',
+	Next=sw_string
+)
+# Edit the below line for the name of the output file/resume
+document.write('[Position1-Company1]name-year.docx')
+print("First resume created")
+# end repeat --------------------------------------------------------
+
+document = MailMerge(template)
+document.merge(
+	Position='[INSERT_POSITION_TITLE]',
+	Company='[INSERT_COMPANY_NAME',
+	Next=cyber_string
+)
+# Edit the below line for the name of the output file/resume
+document.write('[Position2-Company2]name-year.docx')
+print("Second resume created")
+
+document = MailMerge(template)
+document.merge(
+	Position='[INSERT_POSITION_TITLE]',
+	Company='[INSERT_COMPANY_NAME',
+	Next=sw_string
+)
+# Edit the below line for the name of the output file/resume
+document.write('[Position3-Company3]name-year.docx')
+print("Third resume created")
